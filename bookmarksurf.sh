f88d51376bca5eef79433d9cae5ce9ff24926860
akira <akiralifts@gmail.com>
2021-05-25 17:15:24 +0200

    clean up and add search support

--- bookmarksurf.sh
+++ bookmarksurf.sh
@@ -3,9 +3,18 @@ alias browse="$BROWSER"
 if [ "$BROWSER" = "surf" ]; then
 	alias browse="tabbed -r2 $BROWSER -e x"
 fi
-bookmark=$(cat $BOOKMARKS/bookmark_titles | dmenu -i -l 30)
+bookmark=$(sed "a\search" $BOOKMARKS/bookmark_titles | dmenu -i -l 30)
 [[ -n $bookmark ]] || exit 
 idx=$(grep -nF "$bookmark" $BOOKMARKS/bookmark_titles | cut -f1 -d:)
-[[ -n $idx ]] && sed -n "$idx{p}" $BOOKMARKS/bookmarks | browse && exit || 
-	curl -s --head  --request GET "$bookmark" | grep "HTTP" > /dev/null && browse "$bookmark" && exit || 
-	browse "https://duckduckgo.com/?q=""$bookmark"
+if [ "$bookmark" = "search" ]; then
+	engine=$(printf "YouTube\\nDuckDuckGo" | dmenu -i -p "Choose Engine" -l 30)
+	case $engine in
+		YouTube) choice=$(dmenu -i -p "YouTube") && browse "https://www.youtube.com/results?search_query=""$choice" && exit;;
+		DuckDuckGo) choice=$(dmenu -i -p "DuckDuckGo") && browse "https://duckduckgo.com/?q=""$choice" && exit;;
+		Searx) choice=$(dmenu -i -p "Searx") && browse "https:/searx.bar/search?q=""$choice" && exit;;
+	esac
+else 
+	[[ $idx ]] && sed -n "$idx{p}" $BOOKMARKS/bookmarks | browse && exit || 
+		curl -s --head  --request GET "$bookmark" | grep "HTTP" > /dev/null && 
+		browse "$bookmark" && exit || browse "https://duckduckgo.com/?q=""$bookmark" && exit
+fi

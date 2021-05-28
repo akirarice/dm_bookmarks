72f8b8cb462fb22d09afaa26057826c76cc79d54
akira <akiralifts@gmail.com>
2021-05-28 17:37:37 +0200

    removing extra code lines and updating readme

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# My personal scripts for managing bookmarks for any browser.  
+# Bookmark Manager and Meta web browser launcher  
 
--Bookmarker takes what's in the clipboard and adds took bookmark files in ~/.local/bin/bookmarks. This is folder as a env variable in my startup.  
--Bookmarksurf opens browser based on input, should work for most browsers.  
--unbookmarker removes bookmark based on input  
+-Bookmarker takes what's in the clipboard and adds to bookmark files in ~/.local/bin/bookmarks. This folder as an env variable in my startup.  
+-Unbookmarker removes bookmark based on input  
+-Bookmarksurf launches a website based on input. You can also choose 'Search' to pick between search engines and launch your browser with that search. Additionally, I have added the ability to switch browsers to launch with. I added this because sometimes I want to use a full browser like Brave, but usually I'm using surf. Script works with Firefox/Chrome varients, surf, and vimb. Probably works with any browser that takes in text for web launch.
 
-Note that for the demo below, the script runs fast, but the browser does not (suckless surf browser). Brave and other browsers will open at normal speed.
 ![Demo](example.gif)
--- bookmarksurf.sh
+++ bookmarksurf.sh
@@ -5,55 +5,35 @@ while true; do
 	else 
 		bookmark=$(printf "Search\\nSwitch" | dmenu -i -p "Search/URL")
 	fi
-	[[ -n $bookmark ]] || break 
+	[[ -n $bookmark ]] || exit 
 	case $bookmark in
 		Search) 
 			engine=$(printf "YouTube\\nDuckDuckGo\\nSearx\\n1337x\\nbol\\nstartpage\\nBack" | dmenu -i -p "Engine")
 			[[ -n $engine ]] || break 
 			case $engine in
 				YouTube) choice=$(dmenu -i -p "Youtube") 
-					[[ -n $choice ]] && link="https://www.youtube.com/results?search_query=""$choice" || exit
-					break 
-					;; 
+					[[ -n $choice ]] && link="https://www.youtube.com/results?search_query=""$choice" || exit ; break ;; 
 				DuckDuckGo) choice=$(dmenu -i -p "DuckDuckGo") 
-					[[ -n $choice ]] && link="https://duckduckgo.com/?q=""$choice" || exit
-					break
-					;;
+					[[ -n $choice ]] && link="https://duckduckgo.com/?q=""$choice" || exit ; break ;;
 				Searx) choice=$(dmenu -i -p "Searx")
-					[[ -n $choice ]] && link="https://searx.bar/search?q=""$choice" || exit
-					break 
-					;;
+					[[ -n $choice ]] && link="https://searx.bar/search?q=""$choice" || exit ; break ;;
 				1337x) choice=$(dmenu -i -p " 1337x") 
-					[[ -n $choice ]] && link="https://1337x.to/search/""$choice""/1/" || exit
-					break 
-					;;
+					[[ -n $choice ]] && link="https://1337x.to/search/""$choice""/1/" || exit ; break ;;
 				bol) choice=$(dmenu -i -p " bol")
-					[[ -n $choice ]] && link="https://www.bol.com/nl/s/?searchtext=""$choice" || exit
-					break 
-					;;
+					[[ -n $choice ]] && link="https://www.bol.com/nl/s/?searchtext=""$choice" || exit ; break ;;
 				startpage) choice=$(dmenu -i -p " startpage") 
-					[[ -n $choice ]] && link="https://startpage.com/?q=""$choice" || exit
-					break
-					;;
-				Back)  
-					;;
-				*) link="$bookmark" 
-					break
-					;;
-			esac 
-			;; 
-		Switch)  newbrowser=$(printf "brave\\nicecat\\nsurf\\nvimb" | dmenu -i -p "Choose Browser")
-			;;
+					[[ -n $choice ]] && link="https://startpage.com/?q=""$choice" || exit ; break ;;
+				Back) ;;
+				*) link="$bookmark" ; break ;;
+			esac ;; 
+		Switch)  newbrowser=$(printf "brave\\nicecat\\nsurf\\nvimb\\nlibrewolf" | dmenu -i -p "Choose Browser") ;;
 		*) 
 			idx=$(grep -nF "$bookmark" $BOOKMARKS/bookmark_titles | cut -f1 -d:) 
 			if [ "$idx" ]; then 
-				link=$(sed "$idx"'!d' $BOOKMARKS/bookmarks)
-				break
+				link=$(sed "$idx"'!d' $BOOKMARKS/bookmarks) ; break
 			else
-				link="$bookmark"
-				break
-			fi
-			;; 
+				link="$bookmark" ; break
+			fi ;; 
 	esac
 done
 [[ -n $link ]] || exit

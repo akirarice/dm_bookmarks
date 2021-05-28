78c4931db69836b8a29b888c941ace524894dbeb
akira <akiralifts@gmail.com>
2021-05-28 10:10:54 +0200

    Make new script the main one

--- bookmarksurf.sh
+++ bookmarksurf.sh
@@ -1,32 +1,63 @@
 #!/bin/sh
-
-alias browse="$BROWSER"
-if [ "$BROWSER" = "surf" ] || [ "$BROWSER" = "vimb" ]; then
-	alias browse="tabbed -r2 $BROWSER -e x"
-fi
-
-if [ -s $BOOKMARKS/bookmark_titles ]; then
-	bookmark=$(sed "\$a\YouTube\\nDuckDuckGo\\nSearx\\n1337x\\nbol\\nstartpage" $BOOKMARKS/bookmark_titles | dmenu -i -l 15)
-else 
-	bookmark=$(printf "YouTube\\nDuckDuckGo\\nSearx\\n1337x\\nbol\\nstartpage" | dmenu -i -p "Search/URL")
-fi
-
-[[ -n $bookmark ]] || exit
-
-idx=$(grep -nF "$bookmark" $BOOKMARKS/bookmark_titles | cut -f1 -d:)
-
-case $bookmark in
-	YouTube) choice=$(dmenu -i -p "Youtube") && browse "https://www.youtube.com/results?search_query=""$choice" && exit;;
-	DuckDuckGo) choice=$(dmenu -i -p "DuckDuckGo") && browse "https://duckduckgo.com/?q=""$choice" && exit;;
-	Searx) choice=$(dmenu -i -p "Searx") && browse "https://searx.bar/search?q=""$choice" && exit;;
-	1337x) choice=$(dmenu -i -p " 1337x") && browse "https://1337x.to/search/""$choice""/1/" && exit;;
-	bol) choice=$(dmenu -i -p " bol") && browse "https://www.bol.com/nl/s/?searchtext=""$choice" && exit;;
-	startpage) choice=$(dmenu -i -p " startpage") && browse "https://startpage.com/?q=""$choice" && exit;;
-	*) if [ "$idx" ]; then 
-		link=$(sed "$idx"'!d' $BOOKMARKS/bookmarks)
-		browse $link 
+while true; do 
+	if [ -s $BOOKMARKS/bookmark_titles ]; then 
+		bookmark=$(sed "\$a\Search\\nSwitch" $BOOKMARKS/bookmark_titles | dmenu -i -l 15) 
 	else 
-		curl -s --head  --request --fail "$bookmark" && browse "$bookmark" && exit || 
-		browse "https://duckduckgo.com/?q=""$bookmark" && exit
+		bookmark=$(printf "Search\\nSwitch" | dmenu -i -p "Search/URL")
 	fi
-esac
+	[[ -n $bookmark ]] || break 
+	case $bookmark in
+		Search) 
+			engine=$(printf "YouTube\\nDuckDuckGo\\nSearx\\n1337x\\nbol\\nstartpage\\nBack" | dmenu -i -p "Engine")
+			[[ -n $engine ]] || break 
+			case $engine in
+				YouTube) choice=$(dmenu -i -p "Youtube") 
+					[[ -n $choice ]] && link="https://www.youtube.com/results?search_query=""$choice" || exit
+					break 
+					;; 
+				DuckDuckGo) choice=$(dmenu -i -p "DuckDuckGo") 
+					[[ -n $choice ]] && link="https://duckduckgo.com/?q=""$choice" || exit
+					break
+					;;
+				Searx) choice=$(dmenu -i -p "Searx")
+					[[ -n $choice ]] && link="https://searx.bar/search?q=""$choice" || exit
+					break 
+					;;
+				1337x) choice=$(dmenu -i -p " 1337x") 
+					[[ -n $choice ]] && link="https://1337x.to/search/""$choice""/1/" || exit
+					break 
+					;;
+				bol) choice=$(dmenu -i -p " bol")
+					[[ -n $choice ]] && link="https://www.bol.com/nl/s/?searchtext=""$choice" || exit
+					break 
+					;;
+				startpage) choice=$(dmenu -i -p " startpage") 
+					[[ -n $choice ]] && link="https://startpage.com/?q=""$choice" || exit
+					break
+					;;
+				Back)  
+					;;
+				*) link="$bookmark" 
+					break
+					;;
+			esac 
+			;; 
+		Switch)  newbrowser=$(printf "brave\\nicecat\\nsurf\\nvimb" | dmenu -i -p "Choose Browser")
+			;;
+		*) 
+			idx=$(grep -nF "$bookmark" $BOOKMARKS/bookmark_titles | cut -f1 -d:) 
+			if [ "$idx" ]; then 
+				link=$(sed "$idx"'!d' $BOOKMARKS/bookmarks)
+				break
+			else
+				link="$bookmark"
+				break
+			fi
+			;; 
+	esac
+done
+[[ -n $link ]] || exit
+[[ -n $newbrowser ]] || newbrowser="$BROWSER"
+[[ "$newbrowser" = "surf" ]] || [[ "$newbrowser" = "vimb" ]] && newbrowser="tabbed -r2 $newbrowser -e x"
+[[ -n $choice ]] || [[ $idx ]] && $newbrowser "$link" && exit || 
+	curl -s --head  --request --fail "$link" && $newbrowser "$link" || $newbrowser "https://duckduckgo.com/?q=""$link"

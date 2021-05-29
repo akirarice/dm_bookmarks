b792091ce6512ad6094e97132dc83230296c193c
akira <akiralifts@gmail.com>
2021-05-29 10:11:51 +0200

    clean up menus, reduce lines of code

--- bookmarksurf.sh
+++ bookmarksurf.sh
@@ -1,43 +1,18 @@
 #!/bin/sh
 while true; do 
-	if [ -s $BOOKMARKS/bookmark_titles ]; then 
-		bookmark=$(sed "\$a\Search\\nSwitch" $BOOKMARKS/bookmark_titles | dmenu -i -l 15) 
-	else 
-		bookmark=$(printf "Search\\nSwitch" | dmenu -i -p "Search/URL")
-	fi
-	[[ -n $bookmark ]] || exit 
+	bookmark=$(printf "Bookmarks\\nSwitch Browser\\nYouTube\\nSearx\\nbol" | dmenu -i) ; [[ -n $bookmark ]] || exit 
 	case $bookmark in
-		Search) 
-			engine=$(printf "YouTube\\nDuckDuckGo\\nSearx\\n1337x\\nbol\\nstartpage\\nBack" | dmenu -i -p "Engine")
-			[[ -n $engine ]] || break 
-			case $engine in
-				YouTube) choice=$(dmenu -i -p "Youtube") 
-					[[ -n $choice ]] && link="https://www.youtube.com/results?search_query=""$choice" || exit ; break ;; 
-				DuckDuckGo) choice=$(dmenu -i -p "DuckDuckGo") 
-					[[ -n $choice ]] && link="https://duckduckgo.com/?q=""$choice" || exit ; break ;;
-				Searx) choice=$(dmenu -i -p "Searx")
-					[[ -n $choice ]] && link="https://searx.bar/search?q=""$choice" || exit ; break ;;
-				1337x) choice=$(dmenu -i -p " 1337x") 
-					[[ -n $choice ]] && link="https://1337x.to/search/""$choice""/1/" || exit ; break ;;
-				bol) choice=$(dmenu -i -p " bol")
-					[[ -n $choice ]] && link="https://www.bol.com/nl/s/?searchtext=""$choice" || exit ; break ;;
-				startpage) choice=$(dmenu -i -p " startpage") 
-					[[ -n $choice ]] && link="https://startpage.com/?q=""$choice" || exit ; break ;;
-				Back) ;;
-				*) link="$bookmark" ; break ;;
-			esac ;; 
-		Switch)  newbrowser=$(printf "brave\\nicecat\\nsurf\\nvimb\\nlibrewolf" | dmenu -i -p "Choose Browser") ;;
-		*) 
-			idx=$(grep -nF "$bookmark" $BOOKMARKS/bookmark_titles | cut -f1 -d:) 
-			if [ "$idx" ]; then 
-				link=$(sed "$idx"'!d' $BOOKMARKS/bookmarks) ; break
-			else
-				link="$bookmark" ; break
-			fi ;; 
+		YouTube) choice=$(dmenu -i -p "Youtube") ; [[ -n $choice ]] && link="https://www.youtube.com/results?search_query=""$choice" || exit ; break ;;
+		Searx) choice=$(dmenu -p " Searx") ; [[ -n $choice ]] && link="https://searx.bar/search?q=""$choice" || exit ; break ;; 
+		1337x) choice=$(dmenu -p " 1337x") ; [[ -n $choice ]] && link="https://1337x.to/search/""$choice""/1/" || exit ; break ;; 
+		bol) choice=$(dmenu -p " bol") ; [[ -n $choice ]] && link="https://www.bol.com/nl/s/?searchtext=""$choice" || exit ; break ;; 
+		"Switch Browser") newbrowser=$(printf "brave\\nsurf\\nvimb" | dmenu -i -p "Choose Browser") ;; 
+		Bookmarks) choice=$(cat $BOOKMARKS/bookmark_titles | dmenu -i -l 10) ; idx=$(grep -nF "$choice" $BOOKMARKS/bookmark_titles | cut -f1 -d:)  
+			[[ "$idx" ]] && link=$(sed "$idx"'!d' $BOOKMARKS/bookmarks) && break || link="$bookmark" && break ;; 
+		*) link="$bookmark" ; break ;;
 	esac
 done
-[[ -n $link ]] || exit
-[[ -n $newbrowser ]] || newbrowser="$BROWSER"
+[[ -n $link ]] || exit ; [[ -n $newbrowser ]] || newbrowser="$BROWSER"
 [[ "$newbrowser" = "surf" ]] || [[ "$newbrowser" = "vimb" ]] && newbrowser="tabbed -r2 $newbrowser -e x"
 [[ -n $choice ]] || [[ $idx ]] && $newbrowser "$link" && exit || 
 	curl -s --head  --request --fail "$link" && $newbrowser "$link" || $newbrowser "https://duckduckgo.com/?q=""$link"
